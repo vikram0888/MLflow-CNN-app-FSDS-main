ebc5b2d03394b6e9df27443276b2bb48e87c223e
vikram0888 <vikramjha753@gmail.com>
2022-12-19 18:49:24 +0530

    commit

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,10 @@
+#it will do automate the setup of below line of code
+##write the line in terminal to initiate the below line of code --> $bash init_setup.sh
 conda create --prefix ./env python=3.7 -y
 source activate ./env
 pip install -r requirements.txt
 conda env export > conda.yaml
+#after running this if ur still in base env then write this --> conda activate ./env
+
+##local packages -
+#-e . ##this is to install from setup.py
